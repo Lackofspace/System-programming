e0f28b24dcc84931456d540755e4e8de1b03fc86
Denis <86164563+Lackofspace@users.noreply.github.com>
2023-02-14 20:40:08 +0300

    Fixed association between parent and child process

--- linux.cpp
+++ linux.cpp
@@ -21,11 +21,18 @@ string read_string_from_file(const string& filename) {
 
     for (char c : line) {
         if (isalpha(c)) {
-            letters.push_back(c);
+            letters.push_back(tolower(c));
         }
     }
-
     return letters;
+}
+
+int func(string stringLine, size_t start, size_t end, char letter){
+    int sum = accumulate(stringLine.begin() + start, stringLine.begin() + end,
+                            0, [&letter](size_t a, char b) {
+                return (b > letter) ? ++a : a;
+            });
+    return(sum);
 }
 
 int main(int argc, char *argv[]) {
@@ -33,16 +40,15 @@ int main(int argc, char *argv[]) {
         cerr << "Usage: " << argv[0] << " <filename> <N> <letter>" << endl;
         return 1;
     }
-
     string filename = argv[1];
     string stringLine = read_string_from_file(filename);
     if (stringLine.size() < 2) {
-        cerr << "Not enough numbers in file: " << filename << endl;
+        cerr << "Not enough letters in file: " << filename << endl;
         return 1;
     }
 
     int N = atoi(argv[2]);
-    char letter = argv[3][0];
+    char letter = tolower(argv[3][0]);
     vector<pid_t> children(N);
     vector<int> sums(N);
     size_t chunk_size = stringLine.size() / N;
@@ -54,24 +60,20 @@ int main(int argc, char *argv[]) {
         pid_t pid = fork();
         switch(pid){
             case 0:
-                exit(0);
+                sums[i] = func(stringLine, start, end, letter);
+                exit(sums[i]);
             case -1:
                 printf("Error %d\n", errno);
                 exit(1);
             default:
                 children[i] = pid;
-                sums[i] = accumulate(stringLine.begin() + start, stringLine.begin() + end,
-                                     0, [&letter](size_t a, char b) {
-                    return (b > letter) ? ++a : a;
-                });
         }
     }
 
     int result = 0;
     for (int i = 0; i < N; ++i) {
-        waitpid(children[i], nullptr, 0);
-        read(children[i], &sums[i], sizeof(sums[i]));
-        result += sums[i];
+        waitpid(children[i], &sums[i], 0);
+        result += WEXITSTATUS(sums[i]);
     }
 
     cout << "There are " << result << " letters, which are after letter '" << argv[3] << "' in alphabet" << endl;
